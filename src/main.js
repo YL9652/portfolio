8969f16dc32ae388ccb68f980a3c16a94b4b5fb5
Young Lee <YL9652@users.noreply.github.com>
2024-10-07 21:22:37 -0400

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Add a class, header--dark, when scholl down
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
