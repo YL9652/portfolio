ce9eec5f89f9615def0407baf222fd26114d8a76
Young Lee <YL9652@users.noreply.github.com>
2024-10-09 23:00:29 -0400

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -10,10 +10,20 @@ document.addEventListener('scroll', () => {
   }
 });
 
-// Make #Home transparenty
+// Make #Home transparent
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   // console.log(1 - window.scrollY / homeHeight);
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Make Arrow icon display
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = '1';
+  } else {
+    arrowUp.style.opacity = '0';
+  }
 });
