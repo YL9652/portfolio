8f6623da92bd290db6c25f1d313b7b9edef4521b
Young Lee <YL9652@users.noreply.github.com>
2024-10-09 22:05:25 -0400

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 // Add a class, header--dark, when scholl down
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+//const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// Make #Home transparenty
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  // console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
